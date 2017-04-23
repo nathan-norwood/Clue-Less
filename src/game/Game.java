d8f9dc48f2f1e27647d10a62136e73f0d692fe80
schume <schume@users.noreply.github.com>
2017-04-23 11:06:55 -0400

    updated game

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,17 +1,89 @@
 package game;
 
+import java.util.Random;
+import java.util.Vector;
+
 public class Game {
 
 	private int unique_id;
-
+	private Vector<Card> card_deck;
+	private Vector<Card> case_file;
+	
 	public Game(int id){
 		unique_id = id;
+		initCardDeck();	
+		initCaseFile();
 	}
 	
 	public int getId() {
 		return unique_id;
 	}
 
+	private void initCardDeck(){
+		card_deck = new Vector<Card>();
+		int id = 0;
+		card_deck.add(new Card(CardType.SUSPECT, id++, "Miss Scarlet"));
+		card_deck.add(new Card(CardType.SUSPECT, id++, "Colonel Mustard"));
+		card_deck.add(new Card(CardType.SUSPECT, id++, "Professor Plum"));
+		card_deck.add(new Card(CardType.SUSPECT, id++, "Mr. Green"));
+		card_deck.add(new Card(CardType.SUSPECT, id++, "Mrs. White"));
+		card_deck.add(new Card(CardType.SUSPECT, id++, "Mrs. Peacock"));
+		
+		card_deck.add(new Card(CardType.WEAPON, id++, "Rope"));
+		card_deck.add(new Card(CardType.WEAPON, id++, "Lead Pipe"));
+		card_deck.add(new Card(CardType.WEAPON, id++, "Knife"));
+		card_deck.add(new Card(CardType.WEAPON, id++, "Wrench"));
+		card_deck.add(new Card(CardType.WEAPON, id++, "Candlestick"));
+		card_deck.add(new Card(CardType.WEAPON, id++, "Revolver"));
+		
+		card_deck.add(new Card(CardType.ROOM, id++, "Study"));
+		card_deck.add(new Card(CardType.ROOM, id++, "Hall"));
+		card_deck.add(new Card(CardType.ROOM, id++, "Lounge"));
+		card_deck.add(new Card(CardType.ROOM, id++, "Library"));
+		card_deck.add(new Card(CardType.ROOM, id++, "Billiard Room"));
+		card_deck.add(new Card(CardType.ROOM, id++, "Dining Room"));
+		card_deck.add(new Card(CardType.ROOM, id++, "Conservatory"));
+		card_deck.add(new Card(CardType.ROOM, id++, "Ballroom"));
+		card_deck.add(new Card(CardType.ROOM, id++, "Kitchen"));
+	
+		
+	}
+	 
+	private void initCaseFile(){
+		case_file = new Vector<Card>();
+		case_file.add(getNextCardOfType(CardType.SUSPECT));
+		case_file.add(getNextCardOfType(CardType.WEAPON));
+		case_file.add(getNextCardOfType(CardType.ROOM));
+		
+	}
+	
+
+	/*TODO: Get cards and assign them to players... once assigned, deck is empty */
+	public Card getNextCard(){
+		
+		Random randomDeck = new Random();
+		
+		int next = randomDeck.nextInt(card_deck.size());
+		Card card = card_deck.remove(next);
+		return card;
+		
+		
+	}
+	
+	/* TODO: Use this function to select the case file */
+	public Card getNextCardOfType(CardType t){
+		
+		Random randomDeck = new Random();
+		
+		int next = randomDeck.nextInt(card_deck.size());
+		while(card_deck.get(next).getType() != t){
+			next = randomDeck.nextInt(card_deck.size());		
+		}
+		Card card = card_deck.remove(next);
+		return card;
+		
+		
+	}
 
 	public void addPlayer(int playerID){
 		/*TODO: what do we need to send in to add a player to this game?
