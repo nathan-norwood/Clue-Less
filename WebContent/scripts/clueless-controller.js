90db1c4b51ba84da23b1b0a9597e13b93f0c5b28
schume <schume@users.noreply.github.com>
2017-04-26 10:48:34 -0400

    fixed merge

--- WebContent/scripts/clueless-controller.js
+++ WebContent/scripts/clueless-controller.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-var clueless = angular.module('clueless', ['ngCookies', 'ng-websocket']
-).controller('ctrlr', ['$scope', '$cookies', '$websocket', function($scope, $cookies, $websocket){
-=======
+
 var clueless = angular.module('clueless', ['ngCookies', 'angular-websocket']).controller('ctrlr', ['$scope', '$cookies', '$websocket', function($scope, $cookies, $websocket){
->>>>>>> ec7db8b1e2efd7b8e317dcf5c8fa3171eaaaa580
 	
 	$scope.test = "You have passed the test: "+$cookies.get("testCookie");
 	
@@ -14,10 +10,6 @@ var clueless = angular.module('clueless', ['ngCookies', 'angular-websocket']).co
 
 	
 	 // var ws = $websocket('ws://localhost:8080/Clue-Less/socket');
-<<<<<<< HEAD
-	
-=======
->>>>>>> ec7db8b1e2efd7b8e317dcf5c8fa3171eaaaa580
 	var ws = $websocket('ws://localhost:8080/Clue-Less/socket');
 	  //Not sure how to hook this in
 	  ws.onMessage(function(message) {
