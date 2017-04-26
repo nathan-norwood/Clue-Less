ec7db8b1e2efd7b8e317dcf5c8fa3171eaaaa580
nathan-norwood <nathan.norwood22@gmail.com>
2017-04-26 10:45:16 -0400

    change to angular-websocket from ngwebscoket

--- WebContent/scripts/clueless-controller.js
+++ WebContent/scripts/clueless-controller.js
@@ -1,4 +1,4 @@
-var clueless = angular.module('clueless', ['ngCookies', 'angular-websocket']).controller('ctrlr', ['$scope', '$cookies', '$websocket', function($scope, $cookies, $websockets){
+var clueless = angular.module('clueless', ['ngCookies', 'angular-websocket']).controller('ctrlr', ['$scope', '$cookies', '$websocket', function($scope, $cookies, $websocket){
 	
 	$scope.test = "You have passed the test: "+$cookies.get("testCookie");
 	
@@ -9,7 +9,7 @@ var clueless = angular.module('clueless', ['ngCookies', 'angular-websocket']).co
 
 	
 	 // var ws = $websocket('ws://localhost:8080/Clue-Less/socket');
-	var ws = new $websocket('ws://localhost:8080/Clue-Less/socket');
+	var ws = $websocket('ws://localhost:8080/Clue-Less/socket');
 	  //Not sure how to hook this in
 	  ws.onMessage(function(message) {
 	    $scope.test = message;
